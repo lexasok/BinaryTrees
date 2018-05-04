4dac80ebd1ddcfe94365d4c926f8687e4a786433
lexa <sokolov@ozero.net>
2018-05-04 19:59:56 +0300

    Test implemented, main run method declared

--- src/test/java/Testers.java
+++ src/test/java/Testers.java
@@ -1,6 +1,18 @@
 public class Testers {
 
     public static void checkIsEmpty(Tree tree) {
+        if (tree instanceof EmptyBST) {
+            if (tree.isEmpty()) {
+                System.out.println("Good, tree is an EmptyBST and it is empty!\n");
+            }
+        } else if (tree instanceof NonEmptyBST) {
+            if (!tree.isEmpty()) {
+                System.out.println("Good, tree is a NonEmptyBST and it is not empty!\n");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
 
     }
 }
